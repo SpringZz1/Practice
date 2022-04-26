9e019f8fc69c06dbc954b788e899cd342842333e
[200~SpringZz1~ <wang90944@gmail.com>
2022-04-26 18:59:35 +0800

    Meal finder fix

--- Meal-finder/js/script.js
+++ Meal-finder/js/script.js
@@ -1,17 +1,16 @@
-const randomBtn = document.getElementById('random');
-const search = document.getElementById('search');
-const submit = document.getElementById('submit');
-const resultHeading = document.getElementById('result-heading');
-const mealsEl = document.getElementById('meals');
-const singleMealEl = document.getElementById('single-meal');
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    single_mealEl = document.getElementById('single-meal');
 
-/**
- * Search meal and fetch from API
- */
+// Search meal and fetch from API
 function searchMeal(e) {
     e.preventDefault();
 
     // Clear single meal
+    single_mealEl.innerHTML = '';
 
     // Get search term
     const term = search.value;
@@ -22,7 +21,8 @@ function searchMeal(e) {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                resultHeading.innerHTML = `<h2>Search rusults for '${term}'</h2>`;
+                resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
                 if (data.meals === null) {
                     resultHeading.innerHTML = `<p>There are no search results. Try again!<p>`;
                 } else {
@@ -43,52 +43,49 @@ function searchMeal(e) {
         // Clear search text
         search.value = '';
     } else {
-        alert('Please enter a search trim');
+        alert('Please enter a search term');
     }
 }
 
-/**
- * Fetch meal by id
- */
-function getMealByID(mealID) {
+// Fetch meal by ID
+function getMealById(mealID) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
         .then(res => res.json())
         .then(data => {
             const meal = data.meals[0];
 
-            addMealDOM(meal);
+            addMealToDOM(meal);
         });
 }
 
-/**
- * Fetch random meal from API
- */
+// Fetch random meal from API
 function getRandomMeal() {
     // Clear meals and heading
     mealsEl.innerHTML = '';
     resultHeading.innerHTML = '';
+
     fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
         .then(res => res.json())
         .then(data => {
             const meal = data.meals[0];
 
-            addMealDOM(meal);
+            addMealToDOM(meal);
         });
 }
 
-/**
- * Add meal to DOM
- */
-function addMealDOM(meal) {
-    const ingredient = [];
+// Add meal to DOM
+function addMealToDOM(meal) {
+    const ingredients = [];
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 1; i <= 20; i++) {
         if (meal[`strIngredient${i}`]) {
-            ingredient.push(`${meal[`strIngredient${i}`]}-${meal[`strMeasure${i}`]}`);
-        }else{
-            break;
-        }
+            ingredients.push(
+                    `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
     }
+  }
 
   single_mealEl.innerHTML = `
     <div class="single-meal">
@@ -109,22 +106,21 @@ function addMealDOM(meal) {
   `;
 }
 
+// Event listeners
+submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
 
-
-// Add eventlistenr
-submit.addEventListener('click', searchMeal);
-randomBtn.addEventListener('click',getRandomMeal);
-mealsEl.addEventListener('click',e=>{
-    const mealInfo = e.path.find(item=>{
-        if(item.classList){
-            return item.classList.contains('meal-info');
-        }else{
-            return false;
-        }
-    });
-
-    if(mealInfo){
-        const mealID = mealInfo.getAttribute('data-mealid');
-        getMealByID(mealID);
+mealsEl.addEventListener('click', e => {
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
     }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealById(mealID);
+  }
 });
