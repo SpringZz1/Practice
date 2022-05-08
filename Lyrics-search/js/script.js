acef3b220e8ea026c990dcab4c094cc3c5c096a0
SpringZz <wang90944@gmail.com>
2022-05-08 15:09:43 +0800

    New Year Countdown

--- Lyrics-search/js/script.js
+++ Lyrics-search/js/script.js
@@ -10,7 +10,7 @@ const apiURL = 'https://api.lyrics.ovh';
  */
 async function searchSongs(term) {
     const res = await fetch(`${apiURL}/suggest/${term}`);
-    const data = await res.json();
+    const data = res.json();
 
     showData(data);
 }
@@ -21,41 +21,42 @@ async function searchSongs(term) {
 function showData(data) {
     result.innerHTML = `
     <ul class="songs">
-      ${data.data
-        .map(
-          song => `<li>
-      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-    </li>`
-        )
+    ${data.data
+    .map(
+        song=>`
+        <li>
+            <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+            <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>
+        `)
         .join('')}
     </ul>
-  `;
-
-      if (data.prev || data.next) {
-        more.innerHTML = `
-            ${
-                data.prev
-                ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
-                : ''
-            }
-            ${
-                data.next
-                ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
-                : ''
-            }
     `;
-    } else {
-        more.innerHTML = '';
-    }
+
+    if(data.prev||data.next){
+        more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ''
+      }
+    `;
+  } else {
+    more.innerHTML = '';
+  }
 }
 
 /**
  * Get prev and next songs
 */
-async function getMoreSongs(url) {
-    const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
-    const data = await res.json();
+async function getMoreSongs(url){
+    const res=  await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    const data = res.json();
 
     showData(data);
 }
@@ -65,32 +66,30 @@ async function getMoreSongs(url) {
 */
 async function getLyrics(artist,songTitle){
     const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
-    const data = await res.json();
+    const data = res.json();
 
-    if (data.error) {
-        result.innerHTML = data.error;
-    } else {
+    if(data.error){
+        result.innerHTML =data.error;
+    }else{
         const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
 
         result.innerHTML = `
-            <h2><strong>${artist}</strong> - ${songTitle}</h2>
-            <span>${lyrics}</span>
+        <h2><strong>${artist}</strong> - ${songTitle}</h2>
+        <span>${lyrics}</span>
         `;
     }
-
     more.innerHTML = '';
 }
 
 // Event listener
 form.addEventListener('submit',e=>{
     e.preventDefault();
+    const searchTrim =search.value.trim();
 
-    const searchTerm = search.value.trim();
-
-    if(!searchTerm){
+    if(searchTrim){
         alert('Please type in a search term');
     }else{
-        searchSongs(searchTerm);
+        searchSongs(searchTrim);
     }
 });
 
@@ -102,6 +101,6 @@ result.addEventListener('click',e=>{
         const artist = clickEl.getAttribute('data-artist');
         const songTitle = clickEl.getAttribute('data-songtitle');
 
-        getLyrics(artist,songTitle);
+        getLyrics(artist, songTitle);
     }
 });
